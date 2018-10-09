24575ca6ced737b1686fc390c1fdc89fa1192b2b
kacienorton <44007048+kacienorton@users.noreply.github.com>
2018-10-09 15:22:12 -0600

    Add files via upload

---
+++ removeSmall.pl
@@ -0,0 +1,23 @@
+## removesmalls.pl
+## perl removesmalls.pl 200 contigs.fasta > contigs-l200.fasta
+## https://www.biostars.org/p/79202/; Vivek Krishnakumar
+
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+
+my $minlen = shift or die "Error: `minlen` parameter not provided\n";
+{
+    local $/=">";
+    while(<>) {
+        chomp;
+        next unless /\w/;
+        s/>$//gs;
+        my @chunk = split /\n/;
+        my $header = shift @chunk;
+        my $seqlen = length join "", @chunk;
+        print ">$_" if($seqlen >= $minlen);
+    }
+    local $/="\n";
+}
